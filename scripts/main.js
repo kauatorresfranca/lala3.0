a3a6a2f08ce72f35e2a2e073c17b802cf1890197
kaua torres <kauatorresfranca2@gmail.com>
2024-03-09 09:48:22 -0300

    commit inicial

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    $('#button').click(function(e){
+    e.preventDefault()
+    const valor_input = $('#input').val()
+    if (valor_input === 'saudade'){
+        $('#saudade').css('display', 'block');
+    }
+    if (valor_input === 'csa'){
+        $('#csa').css('display', 'block');
+    }
+    if (valor_input === 'fofa'){
+        $('#fofa').css('display', 'block');
+    }
+    if (valor_input === 'praia'){
+        $('#praia').css('display', 'block');
+    }
+    if (valor_input === 'saudade'){
+        $('#saudade').css('display', 'block');
+    }
+    })
+})
