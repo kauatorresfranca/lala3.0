bf90b6cfd22d70a0463fafc31d72423ce8c0593f
kaua torres <kauatorresfranca2@gmail.com>
2024-03-09 14:40:24 -0300

    mudanças

--- scripts/main.js
+++ scripts/main.js
@@ -5,30 +5,37 @@ $(document).ready(function(){
     const valor_input = $('#input').val()
     if (valor_input === 'saudade'){
         $('#saudade').css('display', 'block');
+        alert("Parabéns lalaa, você acertou uma palavra nova e liberou um texto oculto !")
         $('#input').val('')
     }
     else if (valor_input === 'csa'){
         $('#csa').css('display', 'block');
+        alert("Parabéns lalaa, você acertou uma palavra nova e liberou um texto oculto !")
         $('#input').val('')
     }
     else if (valor_input === 'fofa'){
         $('#fofa').css('display', 'block');
+        alert("Parabéns lalaa, você acertou uma palavra nova e liberou um texto oculto !")
         $('#input').val('')
     }
     else if (valor_input === 'praia'){
         $('#praia').css('display', 'block');
+        alert("Parabéns lalaa, você acertou uma palavra nova e liberou um texto oculto !")
         $('#input').val('')
     }
     else if (valor_input === 'tudo'){
         $('#tudo').css('display', 'block');
+        alert("Parabéns lalaa, você acertou uma palavra nova e liberou um texto oculto !")
         $('#input').val('')
     }
     else if (valor_input === 'maturidade'){
         $('#maturidade').css('display', 'block');
+        alert("Parabéns lalaa, você acertou uma palavra nova e liberou um texto oculto !")
         $('#input').val('')
     }
     else if (valor_input === 'eu te amo'){
         $('#te-amo').css('display', 'block');
+        alert("Parabéns lalaa, você acertou uma palavra nova e liberou um texto oculto !")
         $('#input').val('')
     }
     else{
